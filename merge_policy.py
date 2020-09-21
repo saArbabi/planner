b7eacda7161e9f44bd2b641cf1f7f2939f1ccfed
Salar Arbabi <sa00443@surrey.ac.uk>
2020-09-21 21:22:30 +0100

    Add ModelEvaluaiton class

--- merge_policy.py
+++ merge_policy.py
@@ -2,12 +2,12 @@ import numpy as np
 import pandas as pd
 from models.core.train_eval.utils import loadConfig
 from models.core.preprocessing import data_prep
-DataObj = data_prep.DataObj
-
+DataPrep = data_prep.DataPrep
+import json
 from models.core.tf_models import utils
 import matplotlib.pyplot as plt
 from importlib import reload
-
+import os
 from tensorflow import keras
 import warnings
 warnings.filterwarnings('ignore')
@@ -15,87 +15,72 @@ warnings.filterwarnings('ignore')
 
 # %%
 reload(data_prep)
-DataObj = data_prep.DataObj
 
 class GenModel():
     """TODO:
     - reward (A*state + B*Belief)
     - can be attr of the mcts planner
     """
-    def __init__(self):
-        self.steps_n = 10 # time-steps into the future
+    def __init__(self, data_obj):
         self.step_size = 0.1
+        self.scalar_indx = data_obj.scalar_indx
+        self.retain_indx = data_obj.retain_indx
+        self.m_s = data_obj.m_s
+        self.y_s = data_obj.y_s
 
-    def trajCompute(self, state_t0, config):
-        """
-        :Param:
-            state0 - current vehicle state
-        :Return: [[x_cor, y_cor], ...]
-        """
-        state_arr = np.repeat([state_t0], self.samples_n, axis=0)
-        for t in range(self.steps_n):
-            state_arr = self.step(state_arr, t)
-            self.x.append(self.x[-1] + state_arr[:, self.mvehindx['vel']]*0.1)
-            self.y.append(self.y[-1] + act_lat*0.1)
-
-        return self.x, self.y
-
-    def updateArr(self, state_arr, mveh_a, yveh_a):
-        """Updates the state values the result from interacting vehicles taking action
-        """
+    def step(self, state_arr, mveh_a, yveh_a):
         indx = self.retain_indx['vel_mveh']
-            state_arr[:,indx] += mveh_a[:,0]*self.step_size
+        state_arr[:,indx] += mveh_a[:,0]*self.step_size
 
         indx = self.retain_indx['vel_yveh']
-            state_arr[:,indx] += mveh_a[:,0]*self.step_size
+        state_arr[:,indx] += yveh_a*self.step_size
 
-        for state_key in self.mveh_s['boolean']:
-            indx = self.bool_indx[state_key+'_mveh']
-
-        for state_key in self.yveh_s['boolean']:
-            indx = self.bool_indx[state_key+'_yveh']
-
-        for state_key in self.mveh_s['scalar']:
+        for state_key in self.m_s:
             indx = self.scalar_indx[state_key+'_mveh']
             if state_key == 'vel':
                 state_arr[:,indx] += mveh_a[:,0]*self.step_size
             if state_key == 'pc':
-                state_arr[:, indx] += act_lat*self.step_size
-                lc_left = state_arr[:, indx]  > 1.85
-                state_arr[lc_left, indx] = -1.85
-                lc_right = state_arr[:, indx]  < -1.85
-                state_arr[lc_right, indx] = 1.85
-            # if state_key == 'dx':
 
-        for state_key in self.yveh_s['scalar']:
+                state_arr[:, indx] += mveh_a[:,1]*self.step_size
+                lc_left = state_arr[:, indx]  > self.max_lane_width
+                state_arr[lc_left, indx] = self.min_lane_width
+                lc_right = state_arr[:, indx]  < self.min_lane_width
+                state_arr[lc_right, indx] = self.max_lane_width
+
+        for state_key in self.y_s:
             indx = self.scalar_indx[state_key+'_yveh']
+            if state_key == 'vel':
+                state_arr[:,indx] += yveh_a*self.step_size
 
+            if state_key == 'dv':
+                state_arr[:,indx] = state_arr[:, self.retain_indx['vel_mveh']] - \
+                                        state_arr[:, self.retain_indx['vel_yveh']]
 
-    def step(self, state_arr, policy, driver_model):
+            if state_key == 'dx':
+                state_arr[:,indx] += state_arr[:, self.scalar_indx['dv_yveh']]*self.step_size
 
-        for time_step in range(self.steps_n):
-            mveh_a = policy.get_actions(state_arr)
-            yveh_a = driver_model.get_actions(time_step)
+            if state_key == 'da':
+                state_arr[:,indx] = mveh_a[:, 0] - yveh_a
+
+            if state_key == 'a_ratio':
+                state_arr[:,indx] = np.log(np.abs(mveh_a[:, 0]))/ np.abs(yveh_a)
 
         return state_arr
 
 class YieldModel():
-    def __init__(self, config):
+    def __init__(self, data_obj, config):
         # self.loadModel(config)
-        self.data_obj = DataObj(config)
-        self.mvehindx = self.data_obj.mvehindx
-        self.yvehindx = self.data_obj.yvehindx
+        self.data_obj = data_obj
 
+    def get_actions(self, samples_n, time_step):
+
+        return  actions.reshape(actions.shape[1], actions.shape[-1])
 
 class MergePolicy():
-    def __init__(self, config):
+    def __init__(self, data_obj, config):
         self.loadModel(config)
-        self.data_obj = DataObj(config)
-        self.mvehindx = self.data_obj.mvehindx
+        self.data_obj = data_obj
         traj_n = 10
-
-
-        # self.ego = ego
 
     def loadModel(self, config):
         dirName = './models/experiments/'+config['exp_id'] +'/trained_model'
@@ -109,17 +94,132 @@ class MergePolicy():
         """
         state_arr = self.data_obj.applystateScaler(state_arr.reshape(-1, state_arr.shape[-1]))
         parameter_vector = self.model.predict(state_arr)
+        alphas, mus_long, sigmas_long, mus_lat, sigmas_lat, rhos = np.split(parameter_vector[0], 6, axis=0)
+
         action_samples = utils.get_pdf_samples(1, parameter_vector, config['model_type'])
         actions = self.data_obj.applyInvScaler(action_samples)
 
         return  actions.reshape(actions.shape[1], actions.shape[-1])
 
+class ModelEvaluation():
+    dirName = './datasets/preprocessed/'
+
+    def __init__(self, config):
+        self.data_config = config['data_config']
+        self.setup() # load data_obj and validation data
+        self.policy = MergePolicy(self.data_obj, config)
+        self.gen = GenModel(self.data_obj)
+        # self.driver_model = YieldModel(data_obj, config)
+        self.steps_n = 20 # time-steps into the future
+        self.samples_n = 10 # time-steps into the future
+        self.scalar_indx = self.data_obj.scalar_indx
+
+        self.sceneSetup(episode_id=1635)
+
+    def setup(self):
+        config_names = os.listdir(self.dirName+'config_files')
+        for config_name in config_names:
+            with open(self.dirName+'config_files/'+config_name, 'r') as f:
+                config = json.load(f)
+
+            if config == self.data_config:
+                with open(self.dirName+config_name[:-5]+'/'+'data_obj', 'rb') as f:
+                    self.data_obj = pickle.load(f)
+                with open(self.dirName+config_name[:-5]+'/'+'val_m_df', 'rb') as f:
+                    self.val_m_df = pickle.load(f)
+                with open(self.dirName+config_name[:-5]+'/'+'val_y_df', 'rb') as f:
+                    self.val_y_df = pickle.load(f)
+
+    def trajCompute(self):
+        """
+        :Param:
+            state0 - current vehicle state
+        :Return: [[x_cor, y_cor], ...]
+        """
+        state_arr = np.repeat([self.state_t0], self.samples_n, axis=0)
+        x = np.zeros([self.samples_n, self.steps_n])
+        y = np.zeros([self.samples_n, self.steps_n])
+        pc = np.zeros([self.samples_n, self.steps_n])
+        pc[:, 0] = state_arr[:, self.scalar_indx['pc_mveh']]
+
+        for t in range(1, self.steps_n):
+            mveh_a = self.policy.get_actions(state_arr.copy())
+            yveh_a = np.repeat(self.yveh_as[t], self.samples_n, axis=0)
+            x[:, t] = x[:, t-1] + state_arr[:, self.scalar_indx['vel_mveh']]*0.1
+            y[:, t] = y[:, t-1] + mveh_a[:,1]*0.1
+            pc[:, t] = state_arr[:, self.scalar_indx['pc_mveh']]
+            state_arr = self.gen.step(state_arr, mveh_a, yveh_a)
+
+        return x, y, pc
+
+    def sceneSetup(self, episode_id):
+        m_df, y_df = self.data_obj.get_episode_df(self.val_m_df, self.val_y_df, episode_id)
+        v_x_arr, v_y_arr = self.data_obj.get_stateTarget_arr(m_df, y_df)
+        v_x_arr, v_y_arr = self.data_obj.obsSequence(v_x_arr, v_y_arr)
+        self.state_t0 = v_x_arr[0]
+        self.yveh_as = y_df['act_long'].values
+
+        self.gen.min_lane_width = m_df['pc'].min()
+        self.gen.max_lane_width = m_df['pc'].max()
+        x = np.zeros(self.steps_n)
+        y = np.zeros(self.steps_n)
+        vel = m_df['vel'].values
+        act_lat = m_df['act_lat'].values
+        for t in range(1, self.steps_n):
+            x[t] = x[t-1] + vel[t]*0.1
+            y[t] = y[t-1] + act_lat[t]*0.1
+        self.x_true = x
+        self.y_true = y
+        self.pc_true = m_df['pc'].values[0:self.steps_n]
+
+eval = ModelEvaluation(config)
+x, y, pc = eval.trajCompute()
+plt.plot(eval.x_true, eval.y_true, color='red')
+for i in range(10):
+    plt.plot(x[i,:], y[i,:], color='grey')
+
 # %%
-config = loadConfig('exp003')
+for i in range(10):
+    plt.plot(pc[i,:], color='grey')
+    plt.plot(eval.pc_true, color='red')
+
+# %%
+max(eval.pc_true[0:90])
+plt.plot(pc[0])
+plt.plot(eval.pc_true[0:90])
+plt.grid()
+# %%
+
+# %%
+
+a = np.array([1,2,3,4,5,6])
+np.split(a, 2, axis=0)
+import pickle
+
+with open('./datasets/preprocessed/'+'20200921-123920'+'/'+'data_obj', 'rb') as f:
+    data_obj = pickle.load(f)
+
+config = loadConfig('exp001')
+eval = ModelEvaluation(data_obj, config)
+x, y = eval.trajCompute()
+for i in range(10):
+    plt.plot(x[i,:], y[i,:], color='grey')
+x
+# %%
+controller = MergePolicy(data_obj, config)
+
+data_obj.validation_episodes[3]
+m_df, y_df = data_obj.get_episode_df(data_obj.val_m_df, data_obj.val_y_df, 1635)
+v_x_arr, v_y_arr = data_obj.get_stateTarget_arr(m_df, y_df)
+v_x_arr, v_y_arr = data_obj.obsSequence(v_x_arr, v_y_arr)
+controller.get_actions(v_x_arr[0])
+
+# %%
+
 controller = MergePolicy(config)
 
-m_df, y_df = controller.data_obj.get_episode_df(811)
-x_arr, y_arr = controller.data_obj.get_stateTarget_arr(m_df, y_df)
+m_df, y_df = data_prep.get_episode_df(811)
+x_arr, y_arr = data_prep.get_stateTarget_arr(m_df, y_df)
 
 # %%
 
@@ -154,62 +254,7 @@ a
 from importlib import reload
 reload(utils)
 
-# %%
-obs in
-utils.get_pdf_samples
-config = loadConfig('exp003')
-model = loadModel(config)
 
-
-# %%
-# model.predict(x_arr[0])
-
-
-
-
-
-
-    # stateii = statei.copy()
-    stateii = statei.copy()
-    stateii[['a_long_c','v_lat_c']] = a_long_ii, v_lat_ii
-
-    v_long_i = statei['v_long']
-    v_long_ii = v_long_i + a_long_ii*step
-
-
-    ff_long_ii = statei['ff_long'] + statei['ff_v']*step
-    bb_long_ii = statei['bb_long'] + statei['bb_v']*step
-
-    if ff_long_ii < 0:
-        ff_long_ii = 0
-    elif ff_long_ii > 70:
-        ff_long_ii = 70
-
-    if bb_long_ii < 0:
-        bb_long_ii = 0
-    elif bb_long_ii > 70:
-        bb_long_ii = 70
-
-    pc_ii = statei['pc'] + v_lat_ii*step
-    ff_v_ii = frm_ps_val_set['ff_v_long'] - v_long_ii
-    bb_v_ii = v_long_ii - frm_ps_val_set['bb_v_long']
-    stateii['ff_v'] = ff_v_ii
-    stateii['bb_v'] = bb_v_ii
-    stateii['ff_bb_v'] = frm_ps_val_set['ff_bb_v']
-
-    stateii['lc_bool'] = frm_ps_val_set['lc_bool']
-    stateii['frm'] = frm_ps_val_set['frm']
-    stateii[['ff_v_long','bb_v_long']] = frm_ps_val_set[['ff_v_long','bb_v_long']]
-    stateii[['ff_a','bb_a']] = frm_ps_val_set[['ff_a','bb_a']]
-
-    stateii[['a_long_p','v_lat_p']] = a_long_ii, v_lat_ii
-
-    stateii['v_long'] = v_long_ii
-    stateii['ff_long'] = ff_long_ii
-    stateii['bb_long'] = bb_long_ii
-    stateii['pc'] = pc_ii
-
-    return stateii
 
 
 
