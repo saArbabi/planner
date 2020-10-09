42bc87041a4be968d7df4fe7b308565f07adbda1
Salar Arbabi <sa00443@surrey.ac.uk>
2020-10-09 14:42:54 +0100

    Add baseline cae model

--- merge_policy.py
+++ merge_policy.py
@@ -6,18 +6,12 @@ from models.core.tf_models import utils
 import matplotlib.pyplot as plt
 from importlib import reload
 import os
-from tensorflow import keras
-import warnings
-import models.core.tf_models.abstract_model as am
+from models.core.tf_models.cae_model import CAE
 import pickle
 import tensorflow as tf
-import multiprocessing
+import dill
 
 # %%
-
-
-# %%
-
 class GenModel():
     """TODO:
     - reward (A*state + B*Belief)
@@ -69,42 +63,117 @@ class GenModel():
 
         return state_arr
 
-class YieldModel():
-    def __init__(self, data_obj, config):
-        # self.loadModel(config)
-        self.data_obj = data_obj
-
-    def get_actions(self, samples_n, time_step):
-
-        return  actions.reshape(actions.shape[1], actions.shape[-1])
-
 class MergePolicy():
     def __init__(self, data_obj, config):
         self.loadModel(config)
         self.data_obj = data_obj
-        traj_n = 10
+        self.pred_horizon = 20
+        # TODO:
+        # objective function/ evaluate function/ set execution time, which will
+        # execute best ranked traj for a period of time.
 
     def loadModel(self, config):
         checkpoint_dir = './models/experiments/'+config['exp_id'] +'/model_dir'
-        self.model = am.FFMDN(config)
+        self.model = CAE(config)
         Checkpoint = tf.train.Checkpoint(net=self.model)
         # Checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir)).expect_partial()
-        Checkpoint.restore(checkpoint_dir+'/ckpt-10')
+        Checkpoint.restore(checkpoint_dir+'/ckpt-7')
 
-    def get_actions(self, state_arr):
+        self.enc_model = self.model.enc_model
+        self.dec_model = self.model.dec_model
+
+    def set_stepCondition(self, mveh_a, yveh_a):
         """
-        :Param: Unscaled state vector
-        :Return: Control actions for the current state_arr
+        :Return: Conditioning feature for each step.
         """
-        state_arr = self.data_obj.applystateScaler(state_arr.reshape(-1, state_arr.shape[-1]))
-        # parameter_vector = self.model(state_arr, training=False)
-        parameter_vector = self.model(np.append(state_arr, self.fixed_arr_t, axis=1), training=False)
-        alphas, mus_long, sigmas_long, mus_lat, sigmas_lat, rhos = np.split(parameter_vector[0], 6, axis=0)
+        condition_size = 2
+        condition = np.zeros((1, 1, condition_size))
+        condition[0, 0, 0] = mveh_a - yveh_a # da
+        condition[0, 0, 1] = np.log(np.abs(mveh_a)/ np.abs(yveh_a))
+        return condition
 
-        action_samples = utils.get_pdf_samples(1, parameter_vector, config['model_type'])
-        actions = self.data_obj.applyInvScaler(action_samples)
+    def get_actions(self, gmm_m, gmm_y, samples_n):
+        """
+        :Param:
+        - mixtures for both vehicles
+        - number of samples
+        :Return: sampled actions (scaled) for mveh and yveh
+        """
+        mveh_a = gmm_m.sample(samples_n).numpy()
+        yveh_a = gmm_y.sample(samples_n).numpy()
+        return np.reshape(mveh_a, [samples_n, 2]), np.reshape(yveh_a, [samples_n, 1])
 
-        return  actions.reshape(actions.shape[1], actions.shape[-1])
+    def trajCompute(self, obs_seq, condition_t0):
+        """
+        :Param:
+        - sequence of obs
+        - initial condition
+        :Return: sequence of actions for mveh and yveh
+        """
+        mveh_traj = []
+        yveh_traj = []
+
+        enc_state = self.model.enc_model(obs_seq)
+        condition = condition_t0[0]
+        self.dec_model.state = enc_state
+        for step in range(self.pred_horizon):
+            condition.shape = (1, 1, 2)
+            gmm_m, gmm_y = self.model.dec_model([condition ,self.dec_model.state])
+            mveh_a, yveh_a = self.get_actions(gmm_m, gmm_y, 1)
+            mveh_traj.append(mveh_a)
+            yveh_traj.append(yveh_a)
+            if step != (self.pred_horizon - 1):
+                condition = self.set_stepCondition(mveh_a[0][0], yveh_a[0][0])
+                # condition = condition_t0[step+1]
+
+        mveh_traj = np.array(mveh_traj)
+        yveh_traj = np.array(yveh_traj)
+        mveh_traj.shape = (20, 2)
+        yveh_traj.shape = (20, 1)
+        mveh_traj = self.data_obj.apply_InvScaler(mveh_traj)
+        yveh_traj = self.data_obj.apply_InvScaler(yveh_traj)
+
+        return mveh_traj, yveh_traj
+
+    # def trajCompute(self):
+    #     """
+    #     :Param:
+    #         state0 - current vehicle state
+    #     :Return: [[x_cor, y_cor], ...]
+    #     """
+    #     mveh_a = np.zeros([self.samples_n, self.steps_n-1, 2])
+    #     state_arr = np.zeros([self.samples_n, self.steps_n, len(self.state_t0)])
+    #     state_arr[:,0,:] = self.state_t0.copy()
+    #     x = np.zeros([self.samples_n, self.steps_n])
+    #     y = np.zeros([self.samples_n, self.steps_n])
+    #     self.policy.time_stamp = 0
+    #
+    #     for t in range(1, self.steps_n):
+    #         self.policy.ffadj_arr_t = np.repeat([self.ffadj_arr[t-1]], self.samples_n, axis=0)
+    #         # self.policy.ffadj_arr_t = np.repeat([self.ffadj_arr[0]], self.samples_n, axis=0)
+    #         mveh_a[:,t-1,:] = self.policy.get_actions(state_arr[:,t-1,:].copy())
+    #         yveh_a = np.repeat(self.yveh_as[t-1], self.samples_n, axis=0)
+    #         x[:, t] = x[:, t-1] + state_arr[:,t-1,self.scalar_indx['vel_mveh']]*0.1
+    #         y[:, t] = y[:, t-1] + mveh_a[:,t-1,1]*0.1
+    #         state_arr[:,t,:] = self.gen.step(state_arr[:,t-1,:].copy(), mveh_a[:,t-1,:], yveh_a)
+    #         self.policy.time_stamp += 0.1
+    #     return x, y, state_arr, mveh_a
+
+
+    # def get_actions(self, state_arr):
+    #     """
+    #     :Param: Unscaled state vector
+    #     :Return: Control actions for the current state_arr
+    #     """
+    #     state_arr = self.data_obj.applystateScaler(state_arr.reshape(-1, state_arr.shape[-1]))
+    #     # param_vec = self.model(state_arr, training=False)
+    #     param_vec = self.model(np.append(state_arr, self.ffadj_arr_t, axis=1), training=False)
+    #     alphas, mus_long, sigmas_long, mus_lat, sigmas_lat, rhos = np.split(param_vec[0], 6, axis=0)
+    #
+    #     action_samples = utils.get_pdf_samples(1, param_vec, config['model_type'])
+    #     actions = self.data_obj.applyInvScaler(action_samples)
+    #
+    #     return  actions.reshape(actions.shape[1], actions.shape[-1])
 
 class ModelEvaluation():
     dirName = './datasets/preprocessed/'
@@ -113,13 +182,12 @@ class ModelEvaluation():
         self.data_config = config['data_config']
         self.setup() # load data_obj and validation data
         self.policy = MergePolicy(self.data_obj, config)
-        self.gen = GenModel(self.data_obj)
         # self.driver_model = YieldModel(data_obj, config)
         self.steps_n = 50 # time-steps into the future
         self.samples_n = 10 # time-steps into the future
         self.scalar_indx = self.data_obj.scalar_indx
 
-        self.sceneSetup(episode_id=823)
+        self.sceneSetup(episode_id=7)
 
     def setup(self):
         config_names = os.listdir(self.dirName+'config_files')
@@ -129,71 +197,171 @@ class ModelEvaluation():
 
             if config == self.data_config:
                 with open(self.dirName+config_name[:-5]+'/'+'data_obj', 'rb') as f:
-                    self.data_obj = pickle.load(f)
+                    self.data_obj = dill.load(f, ignore=True)
+
                 with open(self.dirName+config_name[:-5]+'/'+'test_m_df', 'rb') as f:
                     self.test_m_df = pickle.load(f)
                 with open(self.dirName+config_name[:-5]+'/'+'test_y_df', 'rb') as f:
                     self.test_y_df = pickle.load(f)
-                with open(self.dirName+config_name[:-5]+'/'+'test_fixed_arr', 'rb') as f:
-                    self.test_fixed_arr = pickle.load(f)
+                with open(self.dirName+config_name[:-5]+'/'+'test_ffadj_arr', 'rb') as f:
+                    self.test_ffadj_arr = pickle.load(f)
 
-    def trajCompute(self):
-        """
-        :Param:
-            state0 - current vehicle state
-        :Return: [[x_cor, y_cor], ...]
-        """
-        mveh_a = np.zeros([self.samples_n, self.steps_n-1, 2])
-        state_arr = np.zeros([self.samples_n, self.steps_n, len(self.state_t0)])
-        state_arr[:,0,:] = self.state_t0.copy()
-        x = np.zeros([self.samples_n, self.steps_n])
-        y = np.zeros([self.samples_n, self.steps_n])
-        self.policy.time_stamp = 0
-
-        for t in range(1, self.steps_n):
-            self.policy.fixed_arr_t = np.repeat([self.fixed_arr[t-1]], self.samples_n, axis=0)
-            # self.policy.fixed_arr_t = np.repeat([self.fixed_arr[0]], self.samples_n, axis=0)
-            mveh_a[:,t-1,:] = self.policy.get_actions(state_arr[:,t-1,:].copy())
-            yveh_a = np.repeat(self.yveh_as[t-1], self.samples_n, axis=0)
-            x[:, t] = x[:, t-1] + state_arr[:,t-1,self.scalar_indx['vel_mveh']]*0.1
-            y[:, t] = y[:, t-1] + mveh_a[:,t-1,1]*0.1
-            state_arr[:,t,:] = self.gen.step(state_arr[:,t-1,:].copy(), mveh_a[:,t-1,:], yveh_a)
-            self.policy.time_stamp += 0.1
-        return x, y, state_arr, mveh_a
+    # def trajCompute(self):
+    #     """
+    #     :Param:
+    #         state0 - current vehicle state
+    #     :Return: [[x_cor, y_cor], ...]
+    #     """
+    #     mveh_a = np.zeros([self.samples_n, self.steps_n-1, 2])
+    #     state_arr = np.zeros([self.samples_n, self.steps_n, len(self.state_t0)])
+    #     state_arr[:,0,:] = self.state_t0.copy()
+    #     x = np.zeros([self.samples_n, self.steps_n])
+    #     y = np.zeros([self.samples_n, self.steps_n])
+    #     self.policy.time_stamp = 0
+    #
+    #     for t in range(1, self.steps_n):
+    #         self.policy.ffadj_arr_t = np.repeat([self.ffadj_arr[t-1]], self.samples_n, axis=0)
+    #         # self.policy.ffadj_arr_t = np.repeat([self.ffadj_arr[0]], self.samples_n, axis=0)
+    #         mveh_a[:,t-1,:] = self.policy.get_actions(state_arr[:,t-1,:].copy())
+    #         yveh_a = np.repeat(self.yveh_as[t-1], self.samples_n, axis=0)
+    #         x[:, t] = x[:, t-1] + state_arr[:,t-1,self.scalar_indx['vel_mveh']]*0.1
+    #         y[:, t] = y[:, t-1] + mveh_a[:,t-1,1]*0.1
+    #         state_arr[:,t,:] = self.gen.step(state_arr[:,t-1,:].copy(), mveh_a[:,t-1,:], yveh_a)
+    #         self.policy.time_stamp += 0.1
+    #     return x, y, state_arr, mveh_a
 
     def sceneSetup(self, episode_id):
         m_df, y_df = self.data_obj.get_episode_df(self.test_m_df, self.test_y_df, episode_id)
-        v_x_arr, v_y_arr = self.data_obj.get_stateTarget_arr(m_df, y_df)
-        self.state_arr_true = np.array(v_x_arr[0:self.steps_n])
-        # v_x_arr, v_y_arr = self.data_obj.obsSequence(v_x_arr, v_y_arr)
-        self.fixed_arr = self.data_obj.get_fixedSate(self.test_fixed_arr, episode_id)
-        self.state_t0 = v_x_arr[0]
-        self.yveh_as = y_df['act_long'].values
+        self.m_id_df = m_df
+        self.y_id_df = y_df
 
-        self.gen.min_lane_width = m_df['pc'].min()
-        self.gen.max_lane_width = m_df['pc'].max()
-        x = np.zeros(self.steps_n)
-        y = np.zeros(self.steps_n)
-        vel = m_df['vel'].values
-        act_lat = m_df['act_lat'].values
-        for t in range(1, self.steps_n):
-            x[t] = x[t-1] + vel[t]*0.1
-            y[t] = y[t-1] + act_lat[t]*0.1
-        self.x_true = x
-        self.y_true = y
-        self.mveh_a_true = m_df[['act_long', 'act_lat']].values[0:self.steps_n-1]
+        state_arr, target_m_arr, target_y_arr, condition_arr = self.data_obj.get_stateTarget_arr(m_df, y_df)
+        state_arr = self.data_obj.applystateScaler(state_arr)
+        target_m_arr = self.data_obj.applytarget_mScaler(target_m_arr)
+        target_y_arr = self.data_obj.applytarget_yScaler(target_y_arr)
+        condition_arr = self.data_obj.applycondition_Scaler(condition_arr)
+        f_x_arr = self.data_obj.get_ffadjState(self.test_ffadj_arr, episode_id)
+        state_arr = np.concatenate([state_arr, f_x_arr], axis=1)
 
-config = loadConfig('series000exp003')
-eval = ModelEvaluation(config)
+        self.state_arr, self.target_m_arr, _,  self.condition_arr = self.data_obj.obsSequence(
+                                    state_arr, target_m_arr, target_y_arr,  condition_arr)
 
-x, y, state_arr, mveh_a = eval.trajCompute()
-plt.plot(eval.x_true, eval.y_true, color='red')
-for i in range(10):
-    plt.plot(x[i,:], y[i,:], color='grey')
+    def trajCompute(self):
+        obs_seq = self.state_arr[0]
+        obs_seq.shape = (1, 20, 15)
+        condition_t0 = np.array(self.condition_arr[0])
+        mveh_traj, yveh_traj = self.policy.trajCompute(obs_seq, condition_t0)
+        self.st = obs_seq
+        self.con = condition_t0
+        # self.state_arr_true = np.array(v_x_arr[0:self.steps_n])
+        # # v_x_arr, v_y_arr = self.data_obj.obsSequence(v_x_arr, v_y_arr)
+        # self.ffadj_arr = self.data_obj.get_ffadjState(self.test_ffadj_arr, episode_id)
+        # self.state_t0 = v_x_arr[0]
+        # self.yveh_as = y_df['act_long'].values
+        #
+        # self.gen.min_lane_width = m_df['pc'].min()
+        # self.gen.max_lane_widt h = m_df['pc'].max()
+        # x = np.zeros(self.steps_n)
+        # y = np.zeros(self.steps_n)
+        # vel = m_df['vel'].values
+        # act_lat = m_df['act_lat'].values
+        # for t in range(1, self.steps_n):
+        #     x[t] = x[t-1] + vel[t]*0.1
+        #     y[t] = y[t-1] + act_lat[t]*0.1
+        # self.x_true = x
+        # self.y_true = y
+        # self.mveh_a_true = m_df[['act_long', 'act_lat']].values[0:self.steps_n-1]
+
+        return mveh_traj, yveh_traj
+
+config = loadConfig('series002exp004')
+eval_obj = ModelEvaluation(config)
+plt.plot(eval_obj.m_id_df['act_long'])
+plt.plot(eval_obj.m_id_df['act_lat'])
+
+# %%
+# %%
+fig, axs = plt.subplots(2, 1)
+for i in range (2):
+    mveh_traj, yveh_traj = eval_obj.trajCompute()
+    axs[0].plot(range(19, 39), mveh_traj[:, 0])
+    axs[1].plot(range(19, 39), yveh_traj)
+
+axs[1].set_xlabel('time')
+axs[0].set_ylabel('mveh_act_long')
+axs[1].set_ylabel('yveh_act_long')
+axs[0].grid(True)
+axs[1].grid(True)
+
+axs[0].plot(eval_obj.m_id_df['act_long'][0:40].values, color='red')
+axs[1].plot(eval_obj.y_id_df['act_long'][0:40].values, color='red')
+
+fig.tight_layout()
+plt.show()
 
 
 # %%
+step = 15
+obs_seq = eval_obj.state_arr[step]
+obs_seq.shape = (1, 20, 15)
+condition_t0 = np.array(eval_obj.condition_arr[step])
+eval_obj.st = obs_seq
+eval_obj.con = condition_t0
+eval_obj.st[0][-1]
+eval_obj.con.shape
+condition_t0[0][0]
+eval_obj.con.shape = (1, 20, 4)
+gmm_m, gmm_y = eval_obj.policy.model([eval_obj.st, eval_obj.con])
+for i in range (10):
+    ma = gmm_m.sample().numpy()
+    ma.shape = (20, 2)
 
+    plt.plot(ma[:, 0], color='grey')
+true = np.array(eval_obj.target_m_arr[step])[:, 0]
+plt.plot(true, color='red')
+plt.title('act_long')
+plt.show()
+# %%
+
+
+# %%
+step = 0
+obs_seq = eval_obj.state_arr[step]
+obs_seq.shape = (1, 20, 15)
+condition_t0 = np.array(eval_obj.condition_arr[step])
+eval_obj.st = obs_seq
+eval_obj.con = condition_t0
+
+eval_obj.con.shape = (1, 20, 4)
+gmm_m, gmm_y = eval_obj.policy.model([eval_obj.st, eval_obj.con])
+for i in range (10):
+    ma = gmm_m.sample().numpy()
+    ma.shape = (20, 2)
+
+    plt.plot(ma[:, 1], color='grey')
+true = np.array(eval_obj.target_m_arr[step])[:, 1]
+plt.plot(true, color='red')
+plt.show()
+
+
+# %%
+ tf.compat.v1.trainable_variables(
+    scope=None)
+# %%
+plt.plot(mveh_traj[:, 1])
+
+# %%
+plt.plot(mveh_traj[:, 0])
+plt.plot(eval_obj.test_m_df['act_long'][20:40].values)
+
+
+# %%
+        self.enc_model = self.model.enc_model
+        self.dec_model = self.model.dec_model
+
+eval_obj.policy.enc_model.trainable_weights[0]
+eval_obj.policy.model.trainable_weights[0]
+eval_obj.policy.dec_model.trainable_weights
 # %%
 """State plots
 """
